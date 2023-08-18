af0d21cd8e0043fac361362cb5f2d02eec9bd786
Abel Landeo <abel.landeo@gmail.com>
2023-08-18 02:45:52 -0500

    minor download file naming improvement

--- server/ipcMainModule.js
+++ server/ipcMainModule.js
@@ -6,14 +6,14 @@ ipcMain.on('init-download', async (event, topicData) => {
     event.sender.send('progress-download', 'Initiating download...');
     let sbClient = new ClientServiceBus(topicData.connectionString, topicData.name, topicData.subsName);
     const dir = app.getPath("downloads");
-    const filePath = `${dir}/${new Date().toJSON()}_${topicData.name}_${topicData.subsName}_${topicData.dlCheck?"dl":"main"}.jsonl`;
+    const filePath = `${dir}/${new Date().toJSON()}__${topicData.name}__${topicData.subsName}__${topicData.dlCheck?"dl":"main"}.jsonl`;
     const fileWritable = fs.createWriteStream(filePath, { flags: "a" });
     let totalMessageCount = 0;
     await sbClient.peekAndProcess(5, topicData.dlCheck, messages => {
         messages.forEach(message => {
             let curatedMessage = {
                 enqueuedTimeUtc: message.enqueuedTimeUtc,
-                deadLetterReson: message.deadLetterReason,
+                deadLetterReason: message.deadLetterReason,
                 deadLetterErrorDescription: message.deadLetterErrorDescription,
                 body: message.body,
                 applicationProperties: message.applicationProperties,
@@ -27,6 +27,7 @@ ipcMain.on('init-download', async (event, topicData) => {
         event.sender.send('progress-download', `${totalMessageCount} messages written`);
     });
     fileWritable.close();
+    await fs.promises.rename(filePath, `${filePath.slice(0, filePath.indexOf(".jsonl"))}__${totalMessageCount}.jsonl`);
     event.sender.send('progress-download', `Complete. ${totalMessageCount} messages saved`);
     new Notification(
         {
